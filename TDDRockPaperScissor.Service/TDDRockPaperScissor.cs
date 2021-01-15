d0a8d1fcb297a46fa9a8422600ca5a6e39913883
b-mannari <b-mannari@accenture.com>
2021-01-15 09:17:29 +0530

    Initial commit

---
+++ TDDRockPaperScissor.Service/TDDRockPaperScissor.cs
@@ -0,0 +1,50 @@
+﻿using static TDDRockPaperScissor.Service.Shapes;
+
+namespace TDDRockPaperScissor.Service
+{
+    public class RockPaperScissor
+    {
+        public GameResult GetTheWinner(Choices p1Choice, Choices p2Choice)
+        {
+            if (p1Choice.Equals(p2Choice))
+            {
+                return GameResult.Tie;
+            }
+
+            if (p1Choice.Equals(Choices.Rock) && p2Choice.Equals(Choices.Paper) ||
+                p1Choice.Equals(Choices.Paper) && p2Choice.Equals(Choices.Scissor) ||
+                p1Choice.Equals(Choices.Scissor) && p2Choice.Equals(Choices.Rock))
+            {
+                return GameResult.Player2_Wins;
+            }
+
+            return GameResult.Player1_Wins;
+        }
+
+        public GameResult GetTheRoundWinner(GameResult outCome1, GameResult outCome2, GameResult outCome3)
+        {
+            if (outCome1.Equals(GameResult.Player1_Wins) && outCome2.Equals(GameResult.Player1_Wins)
+                && (outCome3.Equals(GameResult.Player1_Wins) || outCome3.Equals(GameResult.Player2_Wins)))
+            {
+                return GameResult.Player1_Wins;
+            }
+            if (outCome1.Equals(GameResult.Player1_Wins) && outCome2.Equals(GameResult.Tie) && outCome3.Equals(GameResult.Tie))
+            {
+                return GameResult.Player1_Wins;
+            }
+
+
+            if (outCome1.Equals(GameResult.Player2_Wins) && outCome2.Equals(GameResult.Player2_Wins) &&
+                (outCome3.Equals(GameResult.Player2_Wins) || outCome3.Equals(GameResult.Player1_Wins)))
+            {
+                return GameResult.Player2_Wins;
+            }
+            if (outCome1.Equals(GameResult.Player2_Wins) && outCome2.Equals(GameResult.Tie) && outCome3.Equals(GameResult.Tie))
+            {
+                return GameResult.Player2_Wins;
+            }
+
+            return GameResult.Tie;
+        }
+    }
+}
---
+++ TDDRockPaperScissor.Tests/TDDRockPaperScissorTest.cs
@@ -0,0 +1,144 @@
+using NUnit.Framework;
+using TDDRockPaperScissor.Service;
+using static TDDRockPaperScissor.Service.Shapes;
+
+namespace TDDRockPaperScissor.Tests
+{
+    public class TDDRockPaperScissorTest
+    {
+        RockPaperScissor rps; Choices p1Choice; Choices p2Choice;
+        [SetUp]
+        public void Setup()
+        {
+            rps = new RockPaperScissor();
+        }
+
+
+        [Test]
+        public void ShouldReturnTie_WhenPlayer1ChooseRockAndPlayer2ChooseRock()
+        {
+            p1Choice = Choices.Rock; p2Choice = Choices.Rock; GameResult expectedResult = GameResult.Tie;
+            GameResult actualResult = rps.GetTheWinner(p1Choice, p2Choice);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+        [Test]
+        public void ShouldReturnPlayer1Wins_WhenPlayer1ChooseRockAndPlayer2ChooseScissors()
+        {
+            p1Choice = Choices.Rock; p2Choice = Choices.Scissor; GameResult expectedResult = GameResult.Player1_Wins;
+            GameResult actualResult = rps.GetTheWinner(p1Choice, p2Choice);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+        [Test]
+        public void ShouldReturnPlayer2Wins_WhenPlayer1ChooseRockAndPlayer2ChoosePaper()
+        {
+            p1Choice = Choices.Rock; p2Choice = Choices.Paper; GameResult expectedResult = GameResult.Player2_Wins;
+            GameResult actualResult = rps.GetTheWinner(p1Choice, p2Choice);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+
+
+        [Test]
+        public void ShouldReturnTie_WhenPlayer1ChoosePaperAndPlayer2ChoosePaper()
+        {
+            p1Choice = Choices.Paper; p2Choice = Choices.Paper; GameResult expectedResult = GameResult.Tie;
+            GameResult actualResult = rps.GetTheWinner(p1Choice, p2Choice);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+        [Test]
+        public void ShouldReturnPlayer1Wins_WhenPlayer1ChoosePaperAndPlayer2ChooseRock()
+        {
+            p1Choice = Choices.Paper; p2Choice = Choices.Rock; GameResult expectedResult = GameResult.Player1_Wins;
+            GameResult actualResult = rps.GetTheWinner(p1Choice, p2Choice);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+        [Test]
+        public void ShouldReturnPlayer2Wins_WhenPlayer1ChoosePaperAndPlayer2ChooseScissors()
+        {
+            p1Choice = Choices.Paper; p2Choice = Choices.Scissor; GameResult expectedResult = GameResult.Player2_Wins;
+            GameResult actualResult = rps.GetTheWinner(p1Choice, p2Choice);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+
+
+
+        [Test]
+        public void ShouldReturnTie_WhenPlayer1ChooseScissorsAndPlayer2ChooseScissors()
+        {
+            p1Choice = Choices.Scissor; p2Choice = Choices.Scissor; GameResult expectedResult = GameResult.Tie;
+            GameResult actualResult = rps.GetTheWinner(p1Choice, p2Choice);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+        [Test]
+        public void ShouldReturnPlayer1Wins_WhenPlayer1ChooseScissorsAndPlayer2ChoosePaper()
+        {
+            p1Choice = Choices.Scissor; p2Choice = Choices.Paper; GameResult expectedResult = GameResult.Player1_Wins;
+            GameResult actualResult = rps.GetTheWinner(p1Choice, p2Choice);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+        [Test]
+        public void ShouldReturnPlayer2Wins_WhenPlayer1ChooseScissorsAndPlayer2ChooseRock()
+        {
+            p1Choice = Choices.Scissor; p2Choice = Choices.Rock; GameResult expectedResult = GameResult.Player2_Wins;
+            GameResult actualResult = rps.GetTheWinner(p1Choice, p2Choice);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+
+
+        // //
+        [Test]
+        public void ShouldReturnTie_WhenResultsAreTieForAll3Rounds()
+        {
+            GameResult result = rps.GetTheRoundWinner(GameResult.Tie, GameResult.Tie, GameResult.Tie);
+            Assert.AreEqual(GameResult.Tie, result);
+        }
+
+        [Test]
+        public void ShouldReturnPlayer1Wins_WhenPlayer1IstheWinnerForAll3Rounds()
+        {
+            GameResult result = rps.GetTheRoundWinner(GameResult.Player1_Wins, GameResult.Player1_Wins, GameResult.Player1_Wins);
+            Assert.AreEqual(GameResult.Player1_Wins, result);
+        }
+        [Test]
+        public void ShouldReturnPlayer1Wins_WhenPlayer1IstheWinnerFor2RoundsOutOf3()
+        {
+            GameResult result = rps.GetTheRoundWinner(GameResult.Player1_Wins, GameResult.Player1_Wins, GameResult.Player2_Wins);
+            Assert.AreEqual(GameResult.Player1_Wins, result);
+        }
+        [Test]
+        public void ShouldReturnPlayer1Wins_WhenPlayer1IstheWinnerFor1RoundAndOther2AreTie()
+        {
+            GameResult result = rps.GetTheRoundWinner(GameResult.Player1_Wins, GameResult.Tie, GameResult.Tie);
+            Assert.AreEqual(GameResult.Player1_Wins, result);
+        }
+
+
+
+        [Test]
+        public void ShouldReturnPlayer2Wins_WhenPlayer2IstheWinnerForAll3Rounds()
+        {
+            GameResult result = rps.GetTheRoundWinner(GameResult.Player2_Wins, GameResult.Player2_Wins, GameResult.Player2_Wins);
+            Assert.AreEqual(GameResult.Player2_Wins, result);
+        }
+        [Test]
+        public void ShouldReturnPlayer2Wins_WhenPlayer2IstheWinnerFor2RoundsOutOf3()
+        {
+            GameResult result = rps.GetTheRoundWinner(GameResult.Player2_Wins, GameResult.Player2_Wins, GameResult.Player1_Wins);
+            Assert.AreEqual(GameResult.Player2_Wins, result);
+        }
+        [Test]
+        public void ShouldReturnPlayer2Wins_WhenPlayer2IstheWinnerFor1RoundAndOther2AreTie()
+        {
+            GameResult result = rps.GetTheRoundWinner(GameResult.Player2_Wins, GameResult.Tie, GameResult.Tie);
+            Assert.AreEqual(GameResult.Player2_Wins, result);
+        }
+
+        [OneTimeTearDown]
+        public void TearDown()
+        {
+            rps = null;
+        }
+    }
+}
