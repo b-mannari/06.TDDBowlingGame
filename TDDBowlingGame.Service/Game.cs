a5ae04fcc0b788136b85b7cb7040eb6a5bfcddc5
b-mannari <b-mannari@accenture.com>
2021-01-29 11:20:53 +0530

    Changed the logic of calculating the score

--- TDDBowlingGame.Service/Game.cs
+++ TDDBowlingGame.Service/Game.cs
@@ -1,38 +1,60 @@
-﻿namespace TDDBowlingGame.Service
+﻿using System.Collections.Generic;
+
+namespace TDDBowlingGame.Service
 {
     public class Game
     {
-        private readonly int[] rolls = new int[20];
-        private int CurrentRoll = 0;
+        private readonly int[] rolls = new int[24];
+        public List<int> frameScore = new List<int>();
+        private int CurrentRoll = 0; 
         private int score = 0;
+        int rollNo = 0; 
+        public int CummulativeScore { get; set; }
 
-        public int[] Roll(int PinsDown)
+        public void Roll(int PinsDown)
         {
-            rolls[CurrentRoll++] = PinsDown;
-            return rolls;
+            rolls[CurrentRoll] = PinsDown;
+
+            if (PinsDown == 10)
+            { rolls[CurrentRoll + 1] = 0; CurrentRoll++; }
+
+            CummulativeScore = CalculateScore();
+            CurrentRoll++;
         }
 
-        public int Score()
+        public int CalculateScore()
         {
-            int rollNo = 0;
-            for (int frame = 0; frame < 10; frame++)
+            rollNo = 0; score = 0; frameScore = new List<int>();
+            for (int x = 0; x < 10; x++)
             {
+                bool doubleStrike = false; int framePinsCount;
                 if (rolls[rollNo] == 10) // Strike
                 {
-                    score += 10 + rolls[rollNo + 1] + rolls[rollNo + 2];
-                    rollNo++;
+                    if (rolls[rollNo] + rolls[rollNo + 2] == 20) doubleStrike = true;
+
+                    framePinsCount = rolls[rollNo + 1] + rolls[rollNo + 2] + rolls[rollNo + 3];
+
+                    if (doubleStrike)
+                    { score += 10 + rolls[rollNo + 2] + rolls[rollNo + 3] + rolls[rollNo + 4]; }
+                    else
+                    { score += 10 + rolls[rollNo + 2] + rolls[rollNo + 3]; }
+
                 }
                 else if (rolls[rollNo] + rolls[rollNo + 1] == 10) // Spare
                 {
+                    framePinsCount = rolls[rollNo + 1] + rolls[rollNo + 2];
                     score += 10 + rolls[rollNo + 2];
-                    rollNo += 2;
                 }
-                else
+                else // Normal
                 {
+                    framePinsCount = rolls[rollNo] + rolls[rollNo + 1];
                     score += rolls[rollNo] + rolls[rollNo + 1];
-                    rollNo += 2;
                 }
+                if (framePinsCount > 0) frameScore.Add(score);
+                rollNo += 2;
             }
+            CummulativeScore = score;
+
             return score;
         }
     }
--- TDDBowlingGame.Tests/GameTests.cs
+++ TDDBowlingGame.Tests/GameTests.cs
@@ -1,153 +1,170 @@
 using TDDBowlingGame.Service;
 using NUnit.Framework;
+using System.Collections.Generic;
 
-namespace TDDBowlingGame.Tests 
+namespace TDDBowlingGame.Tests
 {
     public class GameTests
     {
+        public List<int> frameScore = new List<int>();
         private Game g;
+
         [SetUp]
         public void Setup()
         {
             g = new Game();
         }
 
-        private void RollMany(int rolls, int pins)
-        {
-            for (int i = 0; i < rolls; i++)
-            {
-                g.Roll(pins);
-            }
-        }
-
         [Test]
-        public void Should_be_able_to_score_a_game_with_all_Zeros_in_AllEachRoll()
+        public void ShouldReturnScoreZero_WhenWeRollTheBall_And_NoPinsAreDown_InAllRoll()
         {
             RollMany(20, 0);
-            Assert.AreEqual(0, g.Score());
+
+            frameScore = g.frameScore;
+            int result = g.CummulativeScore;
+            Assert.AreEqual(0, result);
         }
 
         [Test]
-        public void ShouldReturnTrue_When1FrameHas1_Rolls()
+        public void ShouldReturnScore_WhenOneRollHasSomePinsAreDown()
         {
             g.Roll(1);
 
-            int result = g.Score();
-
+            frameScore = g.frameScore;
+            int result = g.CummulativeScore;
             Assert.AreEqual(1, result);
         }
 
         [Test]
-        public void ShouldReturnTrue_When1FrameHas2_Rolls()
+        public void ShouldReturnScore_WhenTwoRollsHaveSomePinsAreDown()
         {
             g.Roll(1);
-            g.Roll(4);
-
-            int result = g.Score();
-
-            Assert.AreEqual(5, result);
-        }
-
-        [Test]
-        public void Should_be_able_to_score_a_game_with_One_Spare()
-        {
-            g.Roll(5);
-            g.Roll(5);
-            g.Roll(3);   // Spare from the next Frame
-            //RollMany(17, 0);
-            Assert.AreEqual(16, g.Score());
-        }
-
-        [Test]
-        public void Should_be_able_to_score_a_game_with_One_Strike_1stTest()
-        {
+            g.Roll(2);
             g.Roll(10);
-            //g.Roll(0); //** should not roll **
-            g.Roll(4);
-            g.Roll(5);   // Strike from the next Frame
-            //RollMany(16, 0);
-            Assert.AreEqual(28, g.Score());
-        }
-
-        [Test]
-        public void Should_be_able_to_score_a_game_with_One_Strike_2ndTest()
-        {
-            g.Roll(10);
-            //g.Roll(0); //** should not roll **
+            g.Roll(6);
             g.Roll(3);
-            g.Roll(4); // Strike from the next Frame
-            //RollMany(16, 0);
-            Assert.AreEqual(24, g.Score());
+            frameScore = g.frameScore;
+            int result = g.CummulativeScore;
+            Assert.AreEqual(31, result);
         }
 
-        //[Test]
-        //public void Should_be_a_PerfectGame_10PinsDown_InAll10Frames()
-        //{
-        //    RollMany(12, 10);  //12 is number of rolls and 10 is the number of pins
+        [Test]
+        public void ShouldReturnScoreWithBonus_WithOneSpare()
+        {
+            g.Roll(5);
+            g.Roll(5);
+            g.Roll(3);
 
-        //    Assert.AreEqual(300, g.FrameScore());
-        //}
+            frameScore = g.frameScore;
+            int result = g.CummulativeScore;
+            Assert.AreEqual(16, result);
+        }
 
 
         [Test]
-        public void Should_be_SumOf_2ConsecutiveStrikesin7th8thFrames_FollowedBy2RollsKnockDowninthe9thFrame()
+        public void ShouldReturnScoreWithBonus_WithOneStrike()
+        {
+            g.Roll(10);
+            g.Roll(4);
+            g.Roll(5);
+
+            frameScore = g.frameScore;
+            int result = g.CummulativeScore;
+            Assert.AreEqual(28, result);
+        }
+
+
+        [Test]
+        public void ShouldReturnScoreWithBonus_PerfectGame10PinsDownInAll10Frames()
+        {
+            RollMany(12, 10);
+
+            frameScore = g.frameScore;
+            int result = g.CummulativeScore;
+            Assert.AreEqual(300, result);
+        }
+
+        [Test]
+        public void ShouldReturnScoreWithBonus_WithOneStrike_2ndTest()
+        {
+            g.Roll(0);
+            g.Roll(3);
+            g.Roll(10);
+            g.Roll(0);
+            g.Roll(4);
+
+            frameScore = g.frameScore;
+            int result = g.CummulativeScore;
+            Assert.AreEqual(21, result);
+        }
+
+        [Test]
+        public void ShouldReturnScoreWithBonus_With2ConsecutiveStrikes()
         {
             RollMany(11, 4);
-            RollMany(1, 7);       // Score 51
-            g.Roll(10);    // 1st Strike from Frame 7
-            g.Roll(10);    // 2nd Strike from Frame 8 
-            g.Roll(5);     // Normal pinsdown of next Frame 9
-            g.Roll(3);     // Normal pinsdown of next Frame 9
-            g.Roll(4);     // Normal pinsdown of next Frame 10
-            g.Roll(5);     // Normal pinsdown of next Frame 10
-            Assert.AreEqual(111, g.Score());   //Expected is sum of above
+            RollMany(1, 6);
+            g.Roll(10);
+            g.Roll(10);
+            g.Roll(5);
+            g.Roll(3);
+            g.Roll(4);
+            g.Roll(5);
+
+            frameScore = g.frameScore;
+            int result = g.CummulativeScore;
+            Assert.AreEqual(120, result);
         }
 
         [Test]
-        public void Should_be_SumOf_3ConsecutiveStrikes()
+        public void ShouldReturnScoreWithBonus_With3ConsecutiveStrikes()
         {
             RollMany(11, 4);
-            RollMany(1, 7);       // Score 51
-            g.Roll(10);    // 2nd Strike from Frame 6
-            g.Roll(10);    // 1st Strike from Frame 7
-            g.Roll(10);    // 2nd Strike from Frame 8 
-            g.Roll(3);     // Normal pinsdown of next Frame 9
-            g.Roll(4);     // Normal pinsdown of next Frame 9
-            Assert.AreEqual(128, g.Score());   //Expected is sum of above
+            RollMany(1, 7);
+            g.Roll(10);
+            g.Roll(10);
+            g.Roll(10);
+            g.Roll(3);
+            g.Roll(4);
+
+            frameScore = g.frameScore;
+            int result = g.CummulativeScore;
+            Assert.AreEqual(128, result);
         }
 
 
         [Test]
-        public void Should_be_SumOf_4ConsecutiveStrikes()
+        public void ShouldReturnScoreWithBonus_With4ConsecutiveStrikes()
         {
-            g.Roll(5);    // 2nd Strike from Frame 6
-            g.Roll(5);    // 1st Strike from Frame 7
+            g.Roll(5);
+            g.Roll(5);
 
-            g.Roll(10);    // 2nd Strike from Frame 8 
-            g.Roll(10);    // 2nd Strike from Frame 8 
-            g.Roll(10);     // Normal pinsdown of next Frame 9
-            g.Roll(10);    // 2nd Strike from Frame 8 
+            g.Roll(10);
+            g.Roll(10);
+            g.Roll(10);
+            g.Roll(10);
 
-            g.Roll(4);     // Normal pinsdown of next Frame 9
-            g.Roll(3);    // 2nd Strike from Frame 8 
+            g.Roll(4);
+            g.Roll(3);
 
-            g.Roll(1);     // Normal pinsdown of next Frame 9
-            g.Roll(2);    // 2nd Strike from Frame 8 
+            g.Roll(1);
+            g.Roll(2);
 
-            g.Roll(4);     // Normal pinsdown of next Frame 9
-            g.Roll(6);    // 2nd Strike from Frame 8 
+            g.Roll(4);
+            g.Roll(6);
 
-            g.Roll(4);     // Normal pinsdown of next Frame 9
-            g.Roll(4);    // 2nd Strike from Frame 8 
+            g.Roll(4);
+            g.Roll(4);
 
-            g.Roll(1);     // Normal pinsdown of next Frame 9
-            g.Roll(2);    // 2nd Strike from Frame 8 
+            g.Roll(1);
+            g.Roll(2);
 
-            Assert.AreEqual(156, g.Score());   //Expected is sum of above
+            frameScore = g.frameScore;
+            int result = g.CummulativeScore;
+            Assert.AreEqual(156, result);
         }
 
         [Test]
-        public void ShouldReturnTrue_When10FrameHave2_Rolls()
+        public void ShouldReturnScore_With10FrameHave2_Rolls()
         {
             g.Roll(1);
             g.Roll(4);
@@ -162,7 +179,6 @@ namespace TDDBowlingGame.Tests
             g.Roll(5);
 
             g.Roll(10);
-            //g.Roll(0);
 
             g.Roll(0);
             g.Roll(1);
@@ -174,13 +190,21 @@ namespace TDDBowlingGame.Tests
             g.Roll(4);
 
             g.Roll(10);
-            //g.Roll(0);
 
             g.Roll(2);
             g.Roll(3);
 
-            int result = g.Score();
+            frameScore = g.frameScore;
+            int result = g.CummulativeScore;
             Assert.AreEqual(117, result);
+        }
+
+        private void RollMany(int rolls, int pins)
+        {
+            for (int i = 0; i < rolls; i++)
+            {
+                g.Roll(pins);
+            }
         }
 
         [OneTimeTearDown]
